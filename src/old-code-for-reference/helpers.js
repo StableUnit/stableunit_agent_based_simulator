634a97e06809c5402bccf28f302a4c23770dd100
Igor Barbashin <igor.barbashin@gmail.com>
2018-04-25 14:13:11 -0700

    Remove old code. Add sample tests

--- src/old-code-for-reference/helpers.js
+++
@@ -1,7 +0,0 @@
-export function randomizeByDeviation(value: number, deviation: number): number {
-  return value + value * (Math.random() * 2 - 1) * deviation;
-}
-
-export function accumulateRandomly(value: number, deviation: number): number {
-  return value + value * Math.random() * deviation;
-}
