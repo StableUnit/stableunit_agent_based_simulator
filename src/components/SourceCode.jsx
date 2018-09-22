5083d263a012fc1d0bcb3a8a5b06c571583c0df5
Alexander Bareyko <bareyko@0x.se>
2018-09-22 17:19:41 +0200

    Add description and text

--- src/components/SourceCode.jsx
+++ src/components/SourceCode.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { monoBlue } from 'react-syntax-highlighter/styles/hljs';
 
@@ -9,6 +9,10 @@ type SourceCodeState = {
 
 const highlighterStyles = {
     fontFamily: 'monospace'
+};
+
+const imageStyles = {
+    width: '100%'
 };
 
 class SourceCode extends PureComponent<any, SourceCodeState> {
@@ -30,9 +34,14 @@ class SourceCode extends PureComponent<any, SourceCodeState> {
         }
 
         return (
-            <SyntaxHighlighter showLineNumbers customStyle={highlighterStyles} language="javascript" style={monoBlue}>
-                {file}
-            </SyntaxHighlighter>
+            <Fragment>
+                <p>Text on the top</p>
+                <img style={imageStyles} src="/misc/sim_schema.png" alt="schema"/>
+                <p>Text on the bottom</p>
+                <SyntaxHighlighter showLineNumbers customStyle={highlighterStyles} language="javascript" style={monoBlue}>
+                    {file}
+                </SyntaxHighlighter>
+            </Fragment>
         )
     }
 
